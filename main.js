c93385ee6d6762f7eea0e0b91d28e3f5e9ad1f22
AndreaG <gaggiandrea@gmail.com>
2020-11-28 16:59:53 +0100

    now working with arrow keys

--- main.js
+++ main.js
@@ -3,15 +3,27 @@ $(function () { //document ready
   var previous = $('.prev');
   var next = $('.next');
 
-  next.click(function() {
+  next.click(immagineSuccessiva);
+  previous.click(immaginePrecedente);
+
+  //scorrere le immagini con le frecce della tastiera
+  $(document).keydown(function(event) {
+    if (event.which == 37) {
+      immaginePrecedente();
+    } else if (event.which == 39) {
+      immagineSuccessiva();
+    }
+  });
+
+  function immagineSuccessiva() {
     if ($('img.active').hasClass('last')) {
       $('img.first, img.last, .nav > i.first, .nav > i.last').toggleClass('active');
     } else {
       $('img.active, img.active + img, .nav > i.active, .nav > i.active + i').toggleClass('active');
-    } 
-  });
+    }
+  }
 
-  previous.click(function() {
+  function immaginePrecedente() {
     if ($('img.active').hasClass('first')) {
       $('img.first, img.last, .nav > i.first, .nav > i.last').toggleClass('active');
     } else if ($('img:nth-child(2)').hasClass('active')) {
@@ -20,10 +32,12 @@ $(function () { //document ready
       $('img:nth-child(2), img:nth-child(3), .nav > i:nth-child(2), .nav > i:nth-child(3)').toggleClass('active');
     } else {
       $('img:nth-child(3), img.last, .nav > i:nth-child(3), .nav > i.last').toggleClass('active');
-    } 
-  });
+    }
+  }
+
 
   //BONUS PUNTI DELLA NAV OPERATIVI
+  //vari tentativi cercando di utilizzare this ma niente...
   var bullets = [$('.nav > i.first'), $('.nav > i:nth-child(2)'), $('.nav > i:nth-child(3)'), $('.nav > i.last')];
   bullets[0].click(function() {
     if (!bullets[0].hasClass('active')) {
